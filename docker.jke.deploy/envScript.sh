10dadd0814e75f15844e346ba9b2116a9f163baa
Steve Briand <stbriand@Steves-MacBook-Pro-4.local>
2016-11-30 23:07:02 -0500

    Add Docker environment scritp

---
+++ docker.jke.deploy/envScript.sh
@@ -0,0 +1,4 @@
+# Set env for Docker native commands but deploying to Bluemix
+export DOCKER_HOST=tcp://containers-api.ng.bluemix.net:8443
+export DOCKER_CERT_PATH=/root/.ice/certs/containers-api.ng.bluemix.net/b01d5ffb-cdb4-4e27-ad3c-198db20921e4
+export DOCKER_TLS_VERIFY=1
