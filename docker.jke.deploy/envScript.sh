60cbe0acd63e59f4cfe96e8b6176a6e28fe31fd1
esbriand <esbriand@us.ibm.com>
2017-01-23 11:52:45 -0600

    Add COMPOSE_HTTP_TIMEOUT

--- docker.jke.deploy/envScript.sh
+++ docker.jke.deploy/envScript.sh
@@ -2,3 +2,4 @@
 export DOCKER_HOST=tcp://containers-api.ng.bluemix.net:8443
 export DOCKER_CERT_PATH=/root/.ice/certs/containers-api.ng.bluemix.net/b01d5ffb-cdb4-4e27-ad3c-198db20921e4
 export DOCKER_TLS_VERIFY=1
+export COMPOSE_HTTP_TIMEOUT=120
